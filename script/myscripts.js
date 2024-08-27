2297a54de813c64a4f865b54e851ea23437881b5
CodesWithRobi <rayercodes@gmail.com>
2024-08-27 18:25:14 +0530

    made website and added script

---
+++ script/myscripts.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed, so we add 1
+  const day = String(today.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+
+  document.getElementById("examDate").value = formattedDate;
+});
+function submitForm() {
+  const siteLink = document.getElementById("siteLink").value;
+  const regNumber = encodeURIComponent(
+    document.getElementById("regNumber").value,
+  );
+  const studentName = encodeURIComponent(
+    document.getElementById("studentName").value,
+  );
+  const macAddress = encodeURIComponent(
+    document.getElementById("macAddress").value,
+  );
+  const courseCode = encodeURIComponent(
+    document.getElementById("courseCode").value,
+  );
+  const courseName = encodeURIComponent(
+    document.getElementById("courseName").value,
+  );
+  const mobileNumber = encodeURIComponent(
+    document.getElementById("mobileNumber").value,
+  );
+  const department = encodeURIComponent(
+    document.getElementById("department").value,
+  );
+  const year = encodeURIComponent(document.getElementById("year").value);
+  const semester = encodeURIComponent(
+    document.getElementById("semester").value,
+  );
+  const examDate = encodeURIComponent(
+    document.getElementById("examDate").value,
+  );
+
+  // Format the query string
+  const queryString = `?entry.151300878=${courseCode}&entry.269045572=${regNumber}&entry.470796958=${examDate}&entry.545502880=${year}&entry.619978745=${mobileNumber}&entry.767058731=${macAddress}&entry.1474847017=${studentName}&entry.1661013302=${courseName}&entry.2025981088=${department}&entry.2102436453=${semester}`;
+
+  // Display the result as a clickable link
+  document.getElementById("result").innerHTML =
+    `<a href="${siteLink}${queryString}" target="_blank">Click here</a>`;
+}
